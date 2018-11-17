f13bb10113e3b6255cafb82a61cd472841120a04
leviwp48 <leviwp48@gmail.com>
2018-11-16 21:22:06 -0800

    Initial commit for Flicks2

---
+++ app/src/main/java/com/example/levipole/flicks2/models/Movie.java
@@ -0,0 +1,66 @@
+package com.example.levipole.flicks2.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class Movie {
+
+    int movieId;
+    double voteAverage;
+    String backdropPath;
+    String posterPath;
+    String title;
+    String overview;
+
+    // empty constructor needed by the Parceler Library
+    public Movie() {
+    }
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        backdropPath = jsonObject.getString("backdrop_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+        voteAverage = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException{
+        List<Movie> movies = new ArrayList<>();
+        for(int i = 0; i < movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+}
